2f7483719b50dd4b47deb929dd1e5e7434bd6ee5
Joel Victor <joelsvictor@gmail.com>
2020-02-09 11:53:07 +0530

    Add assertions for in memory db

--- workshop-app/src/workshop_app/db/in_mem.clj
+++ workshop-app/src/workshop_app/db/in_mem.clj
@@ -8,22 +8,26 @@
 (defn create
   "Create an entry for k in our given in memory datastore."
   [c k v]
+  (assert (some? k) "key cannot be nil.")
   (swap! c assoc k v))
 
 
 (defn update
   "Update the entry for k to value v or add it if it doesn't exist in our in memory datastore."
   [c k v]
+  (assert (some? k) "key cannot be nil.")
   (swap! c assoc k v))
 
 
 (defn delete
   "Delete the entry for k in our in memory datastore."
   [c k]
+  (assert (some? k) "key cannot be nil.")
   (swap! c dissoc k))
 
 
 (defn read
   "Read the entry for k in our in memory datastore."
   [c k]
+  (assert (some? k) "key cannot be nil.")
   (get @c k))
