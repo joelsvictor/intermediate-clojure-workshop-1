8b48734d8740e36ed7c1f5aa59fa333cf35ce6ba
Joel Victor <joelsvictor@gmail.com>
2020-02-11 20:54:44 +0530

    Add example for clj to java interop
    Also added group id for the jar

--- workshop-app/project.clj
+++ workshop-app/project.clj
@@ -1,9 +1,10 @@
-(defproject workshop-app "0.1.0"
+(defproject inclojure/workshop-app "0.1.0"
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ring/ring "1.8.0"]
                  [compojure "1.6.1"]
                  [cheshire "5.9.0"]
                  [org.xerial/sqlite-jdbc "3.30.1"]]
+  :aot [workshop-app.clj-to-java-interop]
   :main workshop-app.core
   :global-vars {*warn-on-reflection* true}
   :profiles {:test {:dependencies [[org.clojure/test.check "0.10.0"]]}}
